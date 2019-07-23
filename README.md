d98fe9e230994e59b99bd16da18cf3cfc6f43a65
robin901118 <303278055@qq.com>
2019-07-23 14:37:03 +0800

    1.0.1

---
+++ README.md
@@ -0,0 +1,16 @@
+[![](https://img.shields.io/badge/Github-1.0.1-green.png)](https://github.com/robin901118/i84-cli)
+<br>
+## **简介**
+i84-cli是一个集合vue和cube-ui的二次封装脚手架，适合快速开发，开箱即用。
+<br>
+
+### **安装（全局）**
+```javascript
+npm install i84-cli -g
+````
+
+### **使用**
+```javascript
+i84-cli create
+````
+
---
+++ index.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+
+const version = require('./package').version;//版本号
+
+const program =require("commander");//命令行解析
+
+const createFile = require("./lib/create");//create命令执行库
+
+//开始执行命令
+program
+  .version(version,"-v --version")//设置版本号
+  .command("create")
+  .description("创建页面组件")
+  .action((cmd,options)=>createFile(cmd));
+
+program.parse(process.argv);
+
+
